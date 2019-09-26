56fef1aec0f96a78828cb4453209d4815533bc76
Wildhevire <wildhevire404@gmail.com>
2019-09-26 17:41:44 +0000

    finished

---
+++ src/latihan/pecahan/Pecahan.java
@@ -0,0 +1,19 @@
+package latihan.pecahan;
+
+public class Pecahan {
+	/*
+    Nama        : Wildan Muhammad Fikri
+    Kelas       : IF1
+    NIM         : 10118044
+    Deskripsi   : mendeklarasikan variabel pecahan
+     */
+	public static void main(String[] args) {
+        float f;
+        double d;
+        f = 1234567890.123456789F;
+        d = 1_234_567_890.123456789;
+        System.out.println("Float : " + f);
+        System.out.println("Double : " + d);
+    }
+
+}
